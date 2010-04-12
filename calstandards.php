6d56e0737f32dffbaae4823bc482bee7214611a8
Jonty <jonty@jonty.co.uk>
2010-04-12 18:42:21 +0100

    Now passes every iCal validator I can find, fixes the end-of-month-freakout problem with strtotime.

--- calstandards.php
+++ calstandards.php
@@ -18,14 +18,15 @@ function getMiddleDay($day, $month, $year) {
     return array_shift($potentials);
 }
 
+header('Content-Type: text/calendar');
+ob_start();
 
 // Google calendar explodes if you indent ical. Seriously.
-header('Content-Type: text/calendar');
 ?>
 BEGIN:VCALENDAR
+VERSION:2.0
 PRODID:-//PUBSTANDARDS//PUBCAL 1.0//EN
 CALSCALE:GREGORIAN
-VERSION:2.0
 METHOD:PUBLISH
 X-WR-CALNAME:Pub Standards
 BEGIN:VTIMEZONE
@@ -48,8 +49,15 @@ END:STANDARD
 END:VTIMEZONE
 <?
 
+// Strtotime is dumb when the number of days in the current month is
+// larger than the next, so drop back to the 28th to avoid this
+$currentTime = time();
+if (date('j') > 28) {
+    $currentTime = mktime(date('H'), date('i'), date('s'), date('n'), 28);
+}
+
 for ($i = 0; $i < 100; $i++) {
-    $nextMonth = strtotime("{$i} month");
+    $nextMonth = strtotime("{$i} month", $currentTime);
     $timestamp = getMiddleDay(
         'thursday', 
         date('n', $nextMonth), 
@@ -58,11 +66,14 @@ for ($i = 0; $i < 100; $i++) {
 
 ?>
 BEGIN:VEVENT
-LOCATION:The Bricklayers Arms, Gresse Street, London, W1
+UID:<?=$timestamp?>PS
+DTSTAMP:<?=date('Ymd')?>T<?=date('H')?>0000Z
+ORGANIZER:beer@pubstandards.co.uk
+LOCATION:The Bricklayers Arms\, Gresse Street\, London\, W1
 DTSTART:<?=date('Ymd', $timestamp)?>T180000
 SUMMARY:Pub Standards
 DTEND:<?=date('Ymd', $timestamp)?>T230000
-DESCRIPTION:Beer, lots of beer.
+DESCRIPTION:Beer\, lots of beer.
 END:VEVENT
 <?
 
@@ -70,3 +81,9 @@ END:VEVENT
 
 ?>
 END:VCALENDAR
+<?
+
+# iCal DEMANDS lines end in CRLF. The twat.
+$output = ob_get_contents();
+ob_end_clean();
+print str_replace("\n", "\r\n", $output);
